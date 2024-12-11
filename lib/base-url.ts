f055905001209e47a519a0d198da6d1f23d6d5b3
Loris ALEXANDRE <loris.alexandre66@gmail.com>
2024-12-11 14:59:48 +0100

    change BASE_URL

--- lib/base-url.ts
+++ lib/base-url.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://???.vercel.app";
+    : "https://dataleonlabs-test-frontend.vercel.app";
